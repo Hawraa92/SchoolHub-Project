c1f5afc5086948eb2e3f5a879466a89c5c08034c
Hawraa <hawra92925@gmail.com>
2024-12-27 18:47:59 +0300

    Initial commit

--- SchoolHub/settings.py
+++ SchoolHub/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z0oxuni=denicj4l)**4z&d0$*nh^&hv+9qz#o-d^$8dm$wndx'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -82,8 +83,12 @@ WSGI_APPLICATION = 'SchoolHub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'schoolhub_db',  # اسم قاعدة البيانات
+        'USER': 'postgres',      # اسم المستخدم
+        'PASSWORD': '1234',  # كلمة المرور
+        'HOST': 'localhost',     # العنوان
+        'PORT': '5432',          # المنفذ (الافتراضي هو 5432)
     }
 }
 
